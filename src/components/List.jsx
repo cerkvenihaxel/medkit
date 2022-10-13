08f25e1728123c33b6319be75156e0811d0faf4b
Axel Cerkvenih <cerkvenihaxel@gmail.com>
2022-10-13 20:04:45 -0300

    fixed video error

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -46,8 +46,8 @@ export default function Lists() {
         
     </ul>
 
-    <div class="aspect-w-16 aspect-h-9">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/q9_WM00unRE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <div className="aspect-w-16 aspect-h-9">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/q9_WM00unRE" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
     </div>
 <Button
         href="#"
